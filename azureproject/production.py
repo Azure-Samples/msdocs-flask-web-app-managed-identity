86fdf77e33c6d968e3f4e15a5f2740aa84dd9290
vmagelo <v-gelorminom@microsoft.com>
2023-02-16 21:59:08 +0100

    Fixes for flexible server in tutorial.

--- azureproject/production.py
+++ azureproject/production.py
@@ -8,9 +8,9 @@ ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.env
 CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
 # Configure Postgres database; the full username for PostgreSQL flexible server is
-# username (not @sever-name).
+# username (without @sever-name).
 DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=os.environ['DBUSER'] + "@" + os.environ['DBHOST'],
+    dbuser=os.environ['DBUSER'],
     dbpass='PASSWORDORTOKEN',
     dbhost=os.environ['DBHOST'] + ".postgres.database.azure.com",
     dbname=os.environ['DBNAME']
