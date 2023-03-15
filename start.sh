41b3f539de5773cd0eb3a349604108fbea5fff00
vmagelo <v-gelorminom@microsoft.com>
2023-03-15 14:21:43 +0100

    simplify start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,3 @@
 pip install -r requirements.txt
 
 flask db upgrade
-
-gunicorn --workers 2 --threads 4 --timeout 60 --access-logfile '-' --error-logfile '-' --bind=0.0.0.0:5000 --chdir=/home/site/wwwroot app:app
