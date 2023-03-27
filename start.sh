63bf8771043c89b0ad24b0d2d6655a9dd0967c85
vmagelo <v-gelorminom@microsoft.com>
2023-03-27 16:59:29 +0200

    Add secret_key.

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -v 
 pip install -r requirements.txt
 
+SECRET_KEY=$(python -c 'import secrets; print(secrets.token_hex())')
+
 flask db upgrade
 
 gunicorn --bind=0.0.0.0:8000 --timeout 120 app:app
